f85c0606b86fbeecc86793527b94bfa3afa08da4
jpdarricarrere <jpierred@gmail.com>
2022-12-24 15:35:24 -0300

    fixes ci

--- bar/src/lib.rs
+++ bar/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn add(left: usize, right: usize) -> usize {
-    left + right2
+    left + right
 }
 
 pub fn compiles() {
-		    println!("I compile.")
+    println!("I compile.")
 }
 
 #[cfg(test)]
